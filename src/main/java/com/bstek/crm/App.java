c24e48ab29121687e116a1d6b38d2da077117f1c
Handy <Handy@LAPTOP-B0NKO3BQ>
2017-11-07 11:34:10 +0800

    app

---
+++ src/main/java/com/bstek/crm/App.java
@@ -0,0 +1,13 @@
+package com.bstek.crm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
